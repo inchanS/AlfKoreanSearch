d4961779fb51b34fe958ec112675fa5f2317e5ea
inchanS <song@inchan.dev>
2024-10-17 05:12:10 +0900

    fix: test_workflow.py

--- workflow/test_workflow.py
+++ workflow/test_workflow.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_naver_search(self):
         res = korean_search.get_data('사랑')
 
-        self.assertTrue(len(res['items']) > 0)
+        self.assertTrue(len(res['json']) > 0)
 
 if __name__ == '__main__':
     unittest.main()
